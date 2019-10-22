0731b0967899a1d058b06f26efa6950d5852b1ac
Hefh2 <henning.joelsson@elev.ga.ntig.se>
2019-10-22 12:42:14 +0200

    Created needed files
    Created the files for Main, the class "Pet" and it's children "Extrovert_Easy", "Henning_Medium" and "Introvert_Hard".

---
+++ Raise-a-Henning/Raise-a-Henning/Introvert_Hard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raise_a_Henning
+{
+    class Introvert_Hard : Pet
+    {
+    }
+}
---
+++ Raise-a-Henning/Raise-a-Henning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raise_a_Henning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
